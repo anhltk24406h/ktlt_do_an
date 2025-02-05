cf939fbb26f67bb810014613c3dd8b3a7a7294a9
MCThanh-11 <thanhmc22404a@st.uel.edu.vn>
2025-02-05 17:51:36 +0700

    Add files via upload

---
+++ prepare.py
@@ -0,0 +1,25 @@
+from langchain.text_splitter import RecursiveCharacterTextSplitter, CharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader, DirectoryLoader
+from langchain_community.vectorstores import FAISS
+from langchain_community.embeddings import GPT4AllEmbeddings
+
+# Khai bao bien
+pdf_data_path = "../data"
+vector_db_path = "../vectorstorage/db_faiss"
+
+def create_db_from_files():
+    # Khai bao loader de quet toan bo thu muc dataa
+    loader = DirectoryLoader(pdf_data_path, glob="*.pdf", loader_cls = PyPDFLoader)
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=512, chunk_overlap=50)
+    chunks = text_splitter.split_documents(documents)
+
+    # Embeding
+    embedding_model = GPT4AllEmbeddings(model_file="../lbs/all-MiniLM-L6-v2-f16.gguf")
+    db = FAISS.from_documents(chunks, embedding_model)
+    db.save_local(vector_db_path)
+    return db
+
+
+create_db_from_files()
